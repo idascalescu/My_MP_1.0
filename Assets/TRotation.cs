5ed18b7369d444d7b1889c03b7459029a9de2073
iDasc <100522uk@saeinstitute.edu>
2023-07-15 01:03:45 +0100

    Some Enemies
    Spinning rings enemy / First wave of level 1 prob !

---
+++ Assets/TRotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float rotSpeed;
+
+    public Vector3 rotation;
+
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime * rotSpeed);
+    }
+}
+//Thanks to Morgan for the source code on gitHub for the level 5 team project
